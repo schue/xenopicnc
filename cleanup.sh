de6ead619fab70fedea95150c2a2de190fbdc3cd
Ean Schuessler <ean@brainfood.com>
2015-03-20 21:33:45 -0500

    Remove more cruft

--- cleanup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-apt-get clean 
-rm /var/cache/apt/archive/* /var/cache/apt/* /var/lib/apt/* 2> /dev/null || true
--- doit.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd downloads
-wget https://github.com/raspberrypi/tools/archive/master.tar.gz
-cd ..
-cd lib
-tar xzf ../downloads/master.tar.gz
